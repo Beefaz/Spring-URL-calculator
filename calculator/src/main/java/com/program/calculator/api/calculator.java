02bb4851cbbe0147afd0edbfbe7ace3e0bf13544
Beefaz <55572452+Beefaz@users.noreply.github.com>
2020-04-22 18:13:26 +0300

    Add files via upload
    Fixed 0.1+0.2 wrong result error

--- calculator/src/main/java/com/program/calculator/api/calculator.java
+++ calculator/src/main/java/com/program/calculator/api/calculator.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 
 // + %2B
 // - %2D
@@ -13,15 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class calculator {
     @RequestMapping(value = "/skaiciuoti")
     public String calulation(@RequestParam double sk1, double sk2, char zenklas) {
-        //(zenklas.hashCode()==32)
+        //hashcode of space character is 32
+        //Html changes + symbol into space symbol, so it is changed into hashCode instead.
+        //BigDecimal lines fixes 0.1+0.2 wrong result error
         if (Character.hashCode(zenklas) == 32) {
-            return "Rezultatas: " + (sk1 + sk2);
+            return "Rezultatas: " + BigDecimal.valueOf(sk1).add(BigDecimal.valueOf(sk2), MathContext.UNLIMITED);
         } else if (Character.valueOf('-').equals(zenklas)) {
-            return "Rezultatas: " + (sk1 - sk2);
+            return "Rezultatas: " + BigDecimal.valueOf(sk1).subtract(BigDecimal.valueOf(sk2), MathContext.UNLIMITED);
         } else if (Character.valueOf('*').equals(zenklas)) {
-            return "Rezultatas: " + (sk1 * sk2);
+            return "Rezultatas: " + BigDecimal.valueOf(sk1).multiply(BigDecimal.valueOf(sk2), MathContext.UNLIMITED);
         } else if (Character.valueOf('/').equals(zenklas)) {
-            return "Rezultatas: " + (sk1 / sk2);
+            return "Rezultatas: " + BigDecimal.valueOf(sk1).divide(BigDecimal.valueOf(sk2), MathContext.UNLIMITED);
         }
         return "Kažką ne taip įvedei!!!"+
                 "<BR><BR>Rašyk:"+
